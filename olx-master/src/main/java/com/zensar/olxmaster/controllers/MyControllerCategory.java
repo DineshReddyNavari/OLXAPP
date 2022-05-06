a381989817b0563b624e18f9eb7c0ec2b7f454d8
DN66470 <d.navari@zensar.com>
2022-05-06 16:16:34 +0530

    new commit

--- olx-master/src/main/java/com/zensar/olxmaster/controllers/MyControllerCategory.java
+++ olx-master/src/main/java/com/zensar/olxmaster/controllers/MyControllerCategory.java
@@ -4,25 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zensar.olxmaster.entity.Category;
+import com.zensar.olxmaster.entity.Status;
 
 @RestController
 public class MyControllerCategory {
 	static List<Category> category = new ArrayList<Category>();
-	
+	static List<Status> status = new ArrayList<Status>();
 	static {
 		category.add(new Category(1, "Furniture"));
 		category.add(new Category(2, "RealEstate"));
 		category.add(new Category(3, "Cars"));
 		category.add(new Category(4, "Mobiles"));
 		category.add(new Category(5, "Sports"));
-		
+
 	}
+	static {
+		status.add(new Status(1, "Open"));
+		status.add(new Status(2, "open"));
+		status.add(new Status(3, "closed"));
+		status.add(new Status(4, "closed"));
+		status.add(new Status(5, "open"));
+	}
+
 	@GetMapping("/advertise/category")
 	public List<Category> getAllCategory() {
 		return category;
 	}
+	@GetMapping("/advertise/status")
+	public List<Status> getAllStatus() {
+		return status;
+	}
+	
 
 }
