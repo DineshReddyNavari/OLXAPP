fe546a4d1abf83553cc45be79147fc1e2e2eba3b
DN66470 <d.navari@zensar.com>
2022-05-06 17:36:35 +0530

    commit

--- olx-advertise/repository/StockRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zensar.stockapplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zensar.stockapplication.entity.Stock;
-
-public interface StockRepository extends JpaRepository<Stock, Long>{
-
-}
