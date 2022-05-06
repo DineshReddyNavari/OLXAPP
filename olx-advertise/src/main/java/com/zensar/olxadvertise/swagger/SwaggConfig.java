8d056cfa7dbab0d529a7ced05b4b7f30fc0d4336
DN66470 <d.navari@zensar.com>
2022-05-06 17:37:38 +0530

    commit

--- olx-advertise/src/main/java/com/zensar/olxadvertise/swagger/SwaggConfig.java
+++ olx-advertise/src/main/java/com/zensar/olxadvertise/swagger/SwaggConfig.java
@@ -1,4 +1,4 @@
-package com.zesar.olxadvertise.swagger;
+package com.zensar.olxadvertise.swagger;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
